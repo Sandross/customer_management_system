d506aadb6ea6b631693c4282705fb44e04ae972f
Sandross <sandrosalasfilho3@gmail.com>
2024-03-08 16:14:18 -0300

    feat: docker api

--- src/routes.ts
+++ src/routes.ts
@@ -7,4 +7,8 @@ router.post('/users', (req, res) => {
    return createUserController.handle(req, res);
 });
 
+router.get('/', (req, res) => {
+    return res.json({ message: 'Hello World' });
+});
+
 export { router }
